e17871a87cb0c504f881072d10c5359827ee829e
Shane Breatnach <shane@somethinginterestinghere.com>
2023-06-10 21:40:45 +0100

    Updated image to use latest hugo and netlifyctl releases.

--- images_build.sh
+++ images_build.sh
@@ -2,7 +2,7 @@
 
 docker login
 
-docker build -t sbreatnach/hugo-netlify:1.0.0 ./hugo-netlify
+docker build -t sbreatnach/hugo-netlify:1.1.0 ./hugo-netlify
 docker build -t sbreatnach/hugo-netlify:latest ./hugo-netlify
-docker push sbreatnach/hugo-netlify:1.0.0
+docker push sbreatnach/hugo-netlify:1.1.0
 docker push sbreatnach/hugo-netlify:latest
